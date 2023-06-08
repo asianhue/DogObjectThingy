fac50589bc468f6fbda1cf709c7256317ab6c33f
kwong <kenneth5982@gmail.com>
2023-06-08 10:59:33 -0600

    AB#5702 has been completed

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Testing Message");
         try (Scanner str = new Scanner(System.in)) {
             String color;
             String breed;
