190178fd0addca02946cbb119e1de81376904fb1
kwong <kenneth5982@gmail.com>
2023-06-08 11:03:35 -0600

    Fixed AB#5702

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //asdf
+        //asd
         try (Scanner str = new Scanner(System.in)) {
             String color;
             String breed;
